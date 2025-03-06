060055703323348ae2e2cf9c50da86147d912d24
rama <rama.dwiyantara@uangme.com>
2025-03-06 20:40:02 +0700

    fix(cashier) : make code more cleaner

--- src/modules/cashier/views/CashierUI.vue
+++ src/modules/cashier/views/CashierUI.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
+// Components
+import CashierMainSection from '../components/CashierMainSection.vue';
+
 // interfaces
 import type { ICashierProductProvided } from '../interfaces/cashier-product-service';
 
-// Components
-import CashierListFeaturedProduct from '../components/ListProduct/CashierListFeaturedProduct.vue';
-import CashierListFood from '../components/ListProduct/CashierListFood.vue';
-import CashierListDrink from '../components/ListProduct/CashierListDrink.vue';
-
 // Services
 import { useCashierProductService } from '../services/useCashierProduct.service';
+import CashierOrderSummary from '../components/CashierOrderSummary.vue';
 
 /**
  * @description Destructure all the data and methods what we need
@@ -30,33 +29,9 @@ provide<ICashierProductProvided>('cashierProduct', {
     id="cashier"
     class="grid-wrapper-fullscreen bg-background min-h-screen h-full default-wrapper-fullscreen"
   >
-    <section id="cashier-main-section" class="col-span-8 px-10 py-6">
-      <section id="cashier-content" class="flex flex-col gap-4">
-        <section id="cashier-search-product-category">
-          <!-- TODO: Search product name or category section -->
-        </section>
+    <CashierMainSection />
 
-        <section id="cashier-filter-by-category">
-          <!-- TODO: Filter By Category -->
-        </section>
-
-        <section id="cashier-featured-product">
-          <CashierListFeaturedProduct />
-        </section>
-
-        <section id="cashier-food">
-          <CashierListFood />
-        </section>
-
-        <section id="cashier-drink">
-          <CashierListDrink />
-        </section>
-      </section>
-    </section>
-
-    <section id="order-summary" class="col-span-4 bg-white w-full h-full">
-      <!-- TODO: Order Summary -->
-    </section>
+    <CashierOrderSummary />
   </section>
 </template>
 
