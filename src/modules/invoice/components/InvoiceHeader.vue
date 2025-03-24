d7b91822735172ff7d7f2df22d6afcb0d0d5f906
Rafi Khoirulloh <56441055+Apiiyu@users.noreply.github.com>
2025-03-24 20:12:35 +0700

    feat(invoice): create user interface for invoice page (#12)
    * feat(outlet): create ui for select and create store
    
    * feat: add new form of phone number and upload photo
    
    * feat(eslint): add new rules of eslint and fix several codes
    
    * feat(dashboard): create default layout and update several dependencies version
    
    * feat(dashboard): create ui for dashboard page
    
    * feat(invoice): create ui for invoice page

---
+++ src/modules/invoice/components/InvoiceHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header
+    class="bg-white border border-solid border-grayscale-10 flex items-center justify-center relative inset-0 z-0 px-12 py-6 w-full"
+  >
+    <h1 class="font-semibold text-black text-lg">Invoice Detail</h1>
+
+    <section
+      id="box-icon"
+      class="absolute top-5 right-5 bg-transparent basic-smooth-animation hover:bg-grayscale-10 p-2 rounded-md cursor-pointer"
+    >
+      <AppBaseSvg name="close" />
+    </section>
+  </header>
+</template>
