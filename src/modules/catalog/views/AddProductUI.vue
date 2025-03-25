cf33cde1192aa378f30be9992e31f8c4b41fba63
Daniel Matthew <badutsaurus@gmail.com>
2025-03-26 02:17:19 +0700

    Add Product UI On before leave modal

--- src/modules/catalog/views/AddProductUI.vue
+++ src/modules/catalog/views/AddProductUI.vue
@@ -138,6 +138,31 @@
         </div>
       </div>
     </div>
+    <PrimeVueDialog v-model:visible="isLeavingModal" modal header="">
+      <template #container>
+        <div class="w-[35rem] p-8">
+          <div class="flex flex-col items-center gap-4 text-center">
+            <span><i class="pi pi-trash" style="font-size: 2.5rem"></i></span>
+            <h1 class="text-2xl font-semibold">Are you sure you want to leave this page?</h1>
+            <p>Any changes you make to the data will be lost if you leave this page without saving</p>
+            <div class="flex items-center justify-between gap-4">
+              <PrimeVueButton
+                class="text-lg w-56 text-primary font-semibold"
+                variant="text"
+                label="Discard Changes"
+                @click="confirmLeave"
+              />
+              <PrimeVueButton
+                variant="text"
+                class="w-56 text-lg border-2 border-primary text-primary font-semibold"
+                @click="cancelLeave"
+                >Cancel</PrimeVueButton
+              >
+            </div>
+          </div>
+        </div>
+      </template>
+    </PrimeVueDialog>
   </div>
 </template>
 
@@ -194,6 +219,46 @@ const calculateDiscount = () => {
     }
   }
 };
+
+const nextRoute = ref(null);
+const isLeavingModal = ref(false);
+
+const router = useRouter();
+
+let hasConfirmedLeave = false;
+
+const confirmLeave = () => {
+  isLeavingModal.value = false;
+  hasConfirmedLeave = true;
+
+  if (nextRoute.value) {
+    const targetRoute = nextRoute.value;
+    nextRoute.value = null;
+    router.push(targetRoute);
+  }
+};
+
+const cancelLeave = () => {
+  isLeavingModal.value = false;
+  nextRoute.value = null;
+  hasConfirmedLeave = false;
+};
+
+onBeforeRouteLeave((to, from, next) => {
+  console.log('onBeforeRouteLeave triggered', isLeavingModal.value, hasConfirmedLeave);
+
+  if (hasConfirmedLeave) {
+    hasConfirmedLeave = false;
+    return next();
+  }
+
+  if (!isLeavingModal.value) {
+    isLeavingModal.value = true;
+    nextRoute.value = to.fullPath;
+    next(false);
+  }
+});
+
 watch(product, () => {
   calculateDiscount();
 });
