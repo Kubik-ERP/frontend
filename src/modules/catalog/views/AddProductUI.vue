b57e32c2fc181199c95217a1f5ce307133d11126
badutsaurus <badutsaurus@gmail.com>
2025-03-25 20:42:02 +0700

    Add PrimeVue Dropdown and MultiSelect, replace inputs with PrimeVue components

--- src/modules/catalog/views/AddProductUI.vue
+++ src/modules/catalog/views/AddProductUI.vue
@@ -11,32 +11,84 @@
           label="Select Image"
           icon="pi pi-image"
           class="mt-4 shadow-xs hover:bg-transparent rounded-xl px-8 py-2 text-primary border-primary border-2"
+          :select-all="false"
           variant="outlined"
         />
         <div class="grid grid-cols-2 w-full gap-8 mt-8">
           <div class="flex flex-col">
             <label for="name">Product Name</label>
-            <input id="name" type="text" class="border shadow-xs border-grayscale-30 rounded-lg p-2 w-full" />
+            <PrimeVueInputText
+              id="name"
+              type="text"
+              class="border shadow-xs border-grayscale-30 rounded-lg p-2 w-full"
+            />
           </div>
           <div class="flex flex-col">
             <label for="category">Category</label>
-            <input id="category" type="text" class="border shadow-xs border-grayscale-30 rounded-lg p-2 w-full" />
+            <PrimeVueMultiSelect
+              v-model="selectedCategories"
+              display="chip"
+              :options="categories"
+              filter
+              option-label="name"
+              placeholder="Select"
+              class="w-full text-primary"
+            />
           </div>
           <div class="flex flex-col">
             <label for="price">Price</label>
-            <input id="price" type="number" class="border shadow-xs border-grayscale-30 rounded-lg p-2 w-full" />
+            <PrimeVueInputNumber
+              id="price"
+              type="number"
+              prefix="Rp "
+              fluid
+              class="border shadow-xs border-grayscale-30 rounded-lg"
+            />
           </div>
           <div class="flex items-center gap-2 col-span-2">
             <PrimeVueCheckbox v-model="isDiscount" binary />
             <label for="isDiscount" class="font-bold"> Add Discount Price </label>
           </div>
-          <div class="flex flex-col  col-span-2" :class="isDiscount ? 'block' : 'hidden'">
+          <div class="flex flex-col" :class="isDiscount ? 'block' : 'hidden'">
             <label for="price">Discount Price</label>
-            <input
+            <PrimeVueInputNumber
               id="discount_price"
-              type="number"
-              class="border shadow-xs border-grayscale-30 rounded-lg p-2 w-1/2"
+              class="border w-full shadow-xs border-grayscale-30 rounded-lg"
             />
+          </div>
+          <span></span>
+          <div>
+            <div class="relative w-full">
+              <div class="flex items-center border border-gray-300 rounded-lg overflow-hidden w-full">
+                <!-- Number Input -->
+                <PrimeVueInputNumber
+                  v-model="numberValue"
+                  class="flex-1 ring-0 border-none focus:ring-0"
+                  input-class="w-full px-2 py-1 pr-10"
+                />
+
+                <!-- Dropdown inside the input box (Absolute positioned) -->
+                <div class="absolute right-2 flex items-center">
+                  <PrimeVueDropdown
+                    v-model="selectedDiscountUnit"
+                    :options="['Rp', '%']"
+                    class=" border-none w-fit bg-transparent text-gray-600"
+                    dropdown-icon="pi pi-circle"
+                  >
+                    <template #value="data">
+                      <div>
+                        {{data.value}}
+                      </div>
+                    </template>
+                    <template #option="data">
+                      <div class="w-fit ">
+                        {{data.option}}
+                      </div>
+                    </template>
+                  </PrimeVueDropdown>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -46,6 +98,22 @@
 
 <script setup>
 const isDiscount = ref(false);
+const selectedCategories = ref();
+const categories = ref([
+  { name: 'Category 1' },
+  { name: 'Category 2' },
+  { name: 'Category 3' },
+  { name: 'Category 4' },
+  { name: 'Category 5' },
+  { name: 'Category 6' },
+  { name: 'Category 7' },
+  { name: 'Category 8' },
+  { name: 'Category 9' },
+  { name: 'Category 10' },
+]);
+
+const selectedDiscountUnit = ref()
+
 </script>
 
 <style lang="scss" scoped>
