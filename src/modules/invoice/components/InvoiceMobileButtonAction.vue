aebe7b8fc5bb4a45a1b806a9b204b892bc6c1b10
rama <rama.dwiyantara@uangme.com>
2025-05-15 22:19:48 +0700

    feat(invoice): adjustment mobile responsive on invoice detail

--- src/modules/invoice/components/InvoiceMobileButtonAction.vue
+++ src/modules/invoice/components/InvoiceMobileButtonAction.vue
@@ -3,7 +3,7 @@
   <section id="invoice-mobile-button-action">
     <section
       id="invoice-footer"
-      class="absolute bottom-0 w-full flex flex-col gap-2 p-4 bg-white border-t border-grayscale-10"
+      class="lg:hidden absolute bottom-0 w-full flex flex-col gap-2 p-4 bg-white border-t border-grayscale-10"
     >
       <PrimeVueButton v-slot="slotProps" as-child outlined class="w-full">
         <RouterLink :to="{ name: 'invoice' }" v-bind="slotProps" class="p-3 w-full border border-primary">
