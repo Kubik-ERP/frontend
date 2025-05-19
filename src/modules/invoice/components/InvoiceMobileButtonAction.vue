e102fbb49fd27adf950cb6f981cddb2f45a9e33c
rama.dwiyantara@uangme.com <ramadwiyantara1@gmail.com>
2025-05-19 15:12:50 +0700

    feat(invoice): add print and download functionality

--- src/modules/invoice/components/InvoiceMobileButtonAction.vue
+++ src/modules/invoice/components/InvoiceMobileButtonAction.vue
@@ -1,20 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// emits
+const emit = defineEmits(['print', 'download']);
+</script>
 <template>
   <section id="invoice-mobile-button-action">
     <section
       id="invoice-footer"
       class="lg:hidden absolute bottom-0 w-full flex flex-col gap-2 p-4 bg-white border-t border-grayscale-10"
     >
-      <PrimeVueButton v-slot="slotProps" as-child outlined class="w-full">
-        <RouterLink :to="{ name: 'invoice' }" v-bind="slotProps" class="p-3 w-full border border-primary">
-          <section class="flex gap-2 justify-center w-full items-center">
-            <AppBaseSvg name="download" class="!h-5 !w-5" />
-            <span class="font-semibold text-primary truncate">Download</span>
-          </section>
-        </RouterLink>
+      <PrimeVueButton outlined class="w-full" @click="emit('download')">
+        <section class="flex gap-2 justify-center w-full items-center">
+          <AppBaseSvg name="download" class="!h-5 !w-5" />
+          <span class="font-semibold text-primary truncate">Download</span>
+        </section>
       </PrimeVueButton>
 
-      <PrimeVueButton class="py-2.5 px-14" type="button">
+      <PrimeVueButton class="py-2.5 px-14" type="button" @click="emit('print')">
         <section class="flex gap-2 justify-center w-full items-center">
           <AppBaseSvg name="printer" class="!h-5 !w-5" />
           <span class="font-semibold truncate">Print Invoice</span>
