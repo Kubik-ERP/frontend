713835fd70ddc9c16354b3b2509e43089d5ad672
Apiiyu <khoirulloh.rafi2@gmail.com>
2025-06-10 00:36:00 +0700

    perf: remove unnecessary code

--- src/app/components/layouts/AppLayoutDefault.vue
+++ src/app/components/layouts/AppLayoutDefault.vue
@@ -6,24 +6,19 @@ import AppBaseSidebar from '@/app/components/base/AppBaseSidebar.vue';
 </script>
 
 <template>
-  <Suspense>
-    <section
-      id="default-layout"
-      class="default-wrapper-fullscreen relative inset-0 z-0 grid grid-rows-1 grid-cols-12"
-    >
-      <AppBaseSidebar />
+  <section
+    id="default-layout"
+    class="default-wrapper-fullscreen relative inset-0 z-0 grid grid-rows-1 grid-cols-12"
+  >
+    <AppBaseSidebar />
 
-      <section id="main-layout" class="default-wrapper col-span-10">
-        <AppBaseNavbar />
-        <AppBaseBreadcrumb />
+    <section id="main-layout" class="default-wrapper col-span-10">
+      <AppBaseNavbar />
+      <AppBaseBreadcrumb />
 
-        <section id="main-layout" class="w-full h-fit p-10">
-          <RouterView />
-        </section>
+      <section id="main-layout" class="w-full h-fit p-10">
+        <RouterView />
       </section>
     </section>
-
-    <!-- loading state via #fallback slot -->
-    <template #fallback> Loading... </template>
-  </Suspense>
+  </section>
 </template>
