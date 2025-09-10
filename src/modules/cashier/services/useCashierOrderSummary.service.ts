25d75b14f19d35394ffc38b9b961a5d33559b018
Apiiyu <khoirulloh.rafi2@gmail.com>
2025-09-11 03:36:08 +0700

    fix: conflict changes

--- src/modules/cashier/services/useCashierOrderSummary.service.ts
+++ src/modules/cashier/services/useCashierOrderSummary.service.ts
@@ -186,25 +186,6 @@ export const useCashierOrderSummaryService = (): ICashierOrderSummaryProvided =>
     isLoading: false,
     selectedPaymentMethod: '',
     data: [],
-<<<<<<< HEAD
-=======
-    dataSelfOrder: [
-      {
-        sortNo: 1,
-        id: '1',
-        iconName: 'cash',
-        name: 'Pay at Cashier',
-        isAvailable: true,
-      },
-      {
-        sortNo: 4,
-        id: '4',
-        iconName: 'qris',
-        name: 'QRIS',
-        isAvailable: false,
-      },
-    ],
->>>>>>> d4ff141 (fix: duplicate values on cashier)
   });
 
   const cashierOrderSummary_calculateEstimation = ref<ICashierCalulateEstimationData>({
@@ -334,30 +315,30 @@ export const useCashierOrderSummaryService = (): ICashierOrderSummaryProvided =>
   //     }
   //   };
 
-watch(
-  () => [
-    cashierProduct_selectedProduct.value,
-    cashierOrderSummary_modalVoucher.value.show,
-    cashierOrderSummary_modalVoucher.value.search,
-  ],
-  async () => {
-    if (cashierOrderSummary_modalVoucher.value.show && cashierProduct_selectedProduct.value.length > 0) {
-      debouncedCalculateEstimation();
-      await getVoucherActive(
-        cashierOrderSummary_modalVoucher.value.search,
-        cashierProduct_selectedProduct.value.map(p => p.productId),
-      );
-      if (voucherData.value.length > 0) {
-        // Set default value voucherId dengan voucher pertama
-        cashierOrderSummary_modalVoucher.value.form.voucherId = voucherData.value[0].id;
+  watch(
+    () => [
+      cashierProduct_selectedProduct.value,
+      cashierOrderSummary_modalVoucher.value.show,
+      cashierOrderSummary_modalVoucher.value.search,
+    ],
+    async () => {
+      if (cashierOrderSummary_modalVoucher.value.show && cashierProduct_selectedProduct.value.length > 0) {
+        debouncedCalculateEstimation();
+        await getVoucherActive(
+          cashierOrderSummary_modalVoucher.value.search,
+          cashierProduct_selectedProduct.value.map(p => p.productId),
+        );
+        if (voucherData.value.length > 0) {
+          // Set default value voucherId dengan voucher pertama
+          cashierOrderSummary_modalVoucher.value.form.voucherId = voucherData.value[0].id;
+        }
       }
-    }
 
-    if (cashierOrderSummary_modalVoucher.value.form.voucherId) {
-      debouncedCalculateEstimation();
-    }
-  },
-);
+      if (cashierOrderSummary_modalVoucher.value.form.voucherId) {
+        debouncedCalculateEstimation();
+      }
+    },
+  );
 
   /**
    * @description Handle voucher selection
