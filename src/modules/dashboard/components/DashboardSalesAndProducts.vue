18ee2a1239ffd081b8cc0371d91449887863e9ec
Daniel Matthew <badutsaurus@gmail.com>
2025-09-12 20:36:31 +0700

    merge branch Hotfx/cashier into development (#318)
    * FIX: Cant share whatsapp if customer = null
    
    * FIX: invoice layout movile view
    
    * FEAT: add shortcut cash payment
    
    * FEAT: add payment method report in dashboard
    
    * FIX

--- src/modules/dashboard/components/DashboardSalesAndProducts.vue
+++ src/modules/dashboard/components/DashboardSalesAndProducts.vue
@@ -2,11 +2,15 @@
 // Components
 import DashboardChartLatestSales from './DashboardChartLatestSales.vue';
 import DashboardTopProducts from './DashboardTopProducts.vue';
+import DashboardPaymentMethods from './DashboardPaymentMethods.vue';
 </script>
 
 <template>
   <section id="dashboard-sales-and-products" class="grid grid-rows-1 grid-cols-12 gap-4">
     <DashboardChartLatestSales />
-    <DashboardTopProducts />
+    <section class="flex flex-col gap-4 w-full col-span-full lg:col-span-4 justify-between">
+      <DashboardTopProducts />
+      <DashboardPaymentMethods />
+    </section>
   </section>
 </template>
