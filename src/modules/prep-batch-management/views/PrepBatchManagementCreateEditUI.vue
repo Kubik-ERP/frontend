1d3cb641f0a048f6dbbd41af1cf880134b4bc8f7
Daniel Matthew <badutsaurus@gmail.com>
2025-10-08 17:33:28 +0700

    FEAT: Add Routes Create, Edit, Details

---
+++ src/modules/prep-batch-management/views/PrepBatchManagementCreateEditUI.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// services
+// import { useBatchService } from '../services/prep-batch-management.service';
+
+// const {} = useBatchService();
+</script>
+<template>
+  <div>create edit</div>
+</template>
---
+++ src/modules/prep-batch-management/views/PrepBatchManagementDetailsUI.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// services
+// import { useBatchService } from '../services/prep-batch-management.service';
+
+// const {} = useBatchService();
+</script>
+<template>
+  <div>
+    <div>details</div>
+  </div>
+</template>
