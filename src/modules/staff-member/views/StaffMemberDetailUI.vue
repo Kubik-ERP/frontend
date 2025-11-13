81d06955fb6cd9574103262c682170ec11fcc892
Fikry Ramadhan <115732174+FikryRamadhan@users.noreply.github.com>
2025-11-13 12:54:18 +0700

    Feat/staff point (#507)
    * fix: sidebar-item
    
    * feat (items) : markup kolom in tabel items
    
    * fix (items) adjustmnet at table if retail condition
    
    * Merge branch 'development' of https://github.com/Kubik-ERP/frontend into feat/items-markup
    
    * FEAT: Implement staff member detail view and commissions functionality
    
    * feat : filter at commisions staff

---
+++ src/modules/staff-member/views/StaffMemberDetailUI.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import detailStaff from '../components/detailStaff/index.vue';
+import commissions from '../components/detailStaff/commissions.vue';
+
+import { useStaffMemberDetailService } from '../services/staff-member-detail.service';
+
+const { staffMemberDetails_fetchInformation } = useStaffMemberDetailService();
+onMounted(async () => {
+  await staffMemberDetails_fetchInformation();
+});
+</script>
+<template>
+  <div>
+    <detailStaff />
+    <commissions />
+  </div>
+</template>
