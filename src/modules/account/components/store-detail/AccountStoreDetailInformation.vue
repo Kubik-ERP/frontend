bbe1c61c74ae8de6a2cca0b724d2dafc5478bfcd
Daniel Matthew <badutsaurus@gmail.com>
2025-11-21 00:21:33 +0700

    FIX (Static Invoice) : fix static invoice blocked by BE (#514)

--- src/modules/account/components/store-detail/AccountStoreDetailInformation.vue
+++ src/modules/account/components/store-detail/AccountStoreDetailInformation.vue
@@ -111,7 +111,7 @@ const generateUrl = computed(() => {
           </section>
         </section>
 
-        <section id="form-input" class="col-span-full md:col-span-6 lg:col-span-4 flex flex-col gap-2" v-if="accountStoreDetail_selectedOutlet?.businessType === 'Retail'">
+        <section v-if="accountStoreDetail_selectedOutlet?.businessType === 'Retail'" id="form-input" class="col-span-full md:col-span-6 lg:col-span-4 flex flex-col gap-2">
           <span class="font-normal text-grayscale-70 text-xs">{{ useLocalization('app.self-order-qr-code') }}</span>
 
           <div class="flex items-center gap-2">
